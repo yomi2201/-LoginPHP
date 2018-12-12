fc04ece85388cdbe5904e8c1c9f41116fd24bf48
yomi2201 <brbr091429@yahoo.co.jp>
2018-12-12 13:34:57 +0900

    dotinstall login sample

---
+++ config/init.sql
@@ -0,0 +1,15 @@
+create database dotinstall_sns_php;
+
+grant all on dotinstall_sns_php.* to dbuser@localhost identified by 'mu4uJsif';
+
+use dotinstall_sns_php
+
+create table users (
+  id int not null auto_increment primary key,
+  email varchar(255) unique,
+  password varchar(255),
+  created datetime,
+  modified datetime
+);
+
+desc users;
